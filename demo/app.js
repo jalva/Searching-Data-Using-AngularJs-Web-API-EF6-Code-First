aa902e4bbd95a043808e794da69a85c5e1d4fa1b
Jorge <jalva01@gmail.com>
2018-11-02 14:44:09 -0400

    ...

---
+++ demo/app.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('searchFiltersApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    
+    $routeProvider.when("", {
+        redirectTo: "/book"
+    });
+    $routeProvider.when("/", {
+        redirectTo: "/book"
+    });
+    $routeProvider.when("/book", {
+        template: '<search-root columns="$resolve.columns"></search-root>',
+        resolve: {
+            columns: function (schemaService) {
+                return schemaService.getColumns();
+            }
+        },
+        reloadOnSearch: false
+    });
+
+});
+
+app.constant('searchUrl', '/demo/api/books.json');
+app.constant('getFiltersUrl', '/demo/api/filters.json');
+app.constant('getColumnsUrl', '/demo/api/schema.json');
+
